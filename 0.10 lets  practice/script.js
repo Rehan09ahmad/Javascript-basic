803c0bf24f0283ddcd6c18d75c58f5387827c06d
rehan <rehanahmad3775@gmail.com>
2025-12-18 22:03:14 +0500

    add practice folder

---
+++ 0.10 lets  practice/script.js
@@ -0,0 +1,15 @@
+let score = 90;
+let grade ;
+
+if (score >= 90 && score <= 100){
+    grade = "A"
+}else if (score >= 70 && score <= 89){
+    grade = "B"
+}else if (score >= 60 && score <= 69){
+    grade = "C"
+}else if (score >= 50 && score <= 59){
+    graade = "D"
+}else if (score >= 0 && score <= 49){
+
+}
+console.log("accourding to your score grade was:",grade )
