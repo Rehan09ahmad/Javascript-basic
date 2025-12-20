f3bbb9e55b001504c0018e4f947e076f325b630a
rehan <rehanahmad3775@gmail.com>
2025-12-20 11:05:26 +0500

    Added loops and strings basics, starting with for loop

--- 11-loops-and-strings/loops/script.js
+++
@@ -1,7 +0,0 @@
-// Loops 
-
-for(let counts = 1; counts <= 5; counts++){
-    console.log("Hy there I'm Rehan Ahamd ")
-}
-
-
---
+++ 11-loops-and-strings/script.js
@@ -0,0 +1,5 @@
+// for loop
+
+for(let i = 1; i <= 5; i++){
+    console.log("Hy there im Rehan Ahamd ")
+}
