f707392d3672785e91dd6bc05b93e9cf56f9671a
rehan <rehanahmad3775@gmail.com>
2025-12-20 11:35:57 +0500

    Added loops and strings basics, starting with for loop

--- 11-loops-and-strings/script.js
+++ 11-loops-and-strings/script.js
@@ -1,5 +1,15 @@
-// for loop
-
+// // for loop
+{
 for(let i = 1; i <= 5; i++){
     console.log("Hy there im Rehan Ahamd ")
+}
+}
+{
+
+    // calculate sum of 1 to 5 
+    let sum = 0;
+    for(let count = 1; count <= 10; count ++){
+        sum = sum + count;
+    }
+       console.log("sum =",sum)
 }
