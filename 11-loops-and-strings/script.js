ac4f29c44ce18003a1218d7db26a15ec57adc72c
rehan <rehanahmad3775@gmail.com>
2025-12-20 11:52:31 +0500

    Counting / Iterative for loop

--- 11-loops-and-strings/script.js
+++ 11-loops-and-strings/script.js
@@ -1,14 +1,15 @@
 // // for loop
 {
 for(let i = 1; i <= 5; i++){
-    console.log("Hy there im Rehan Ahamd ")
+    console.log("Hy there im Rehan Ahamd")
 }
 }
 {
 
-    // calculate sum of 1 to 5 
+    // calculate sum of 1 to 100 
     let sum = 0;
-    for(let count = 1; count <= 10; count ++){
+    let  n = 100;
+    for(let count = 1; count <= n; count ++){
         sum = sum + count;
     }
        console.log("sum =",sum)
---
+++ 11-loops-and-strings/second.js
@@ -0,0 +1,5 @@
+// 
+
+for(let i = 1; i <= 5; i++){
+    console.log("Hy there I'm Rehan Ahmad", i)
+}
