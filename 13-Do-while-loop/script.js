acd8ba2d6d3accdbf2b9fc6ca7938b4fee2be885
rehan <rehanahmad3775@gmail.com>
2025-12-21 22:24:08 +0500

    added do while loop folder

---
+++ 13-Do-while-loop/script.js
@@ -0,0 +1,20 @@
+{
+    let i = 20;
+do{
+    console.log("Rehan Ahmad")
+    i++;
+}while(i <= 10);
+}
+
+
+// ======================================
+
+
+
+{
+    let i = 1;
+    do{
+        console.log("i=",i)
+        i++
+    }while(i <= 10);
+}
