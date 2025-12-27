e13d9ada7cb87bb68033f3a3816072173f41136b
rehan <rehanahmad3775@gmail.com>
2025-12-27 18:47:29 +0500

    practice Question

--- 18-string-in-js/script.js
+++ 18-string-in-js/script.js
@@ -1,10 +1,22 @@
+// {
+//     let Name = "Rehan Ahmad";
+//     console.log(Name.length)   // {Length} Counts the total number of characters in a string (spaces are included).
+// }
+
+
+// {
+//     let str = "im rehan ahmad ";
+//     console.log(str.slice(0,13)) // {slice} Used to extract a part of a string.
+// }
+
+
+
+
 {
-    let Name = "Rehan Ahmad";
-    console.log(Name.length)
+    let str = "rehan ahmad";
+
+for (let i = 0; i < str.length; i++) {
+  console.log("index",i,"character",str[i]);
 }
 
-
-{
-    let str = "im rehan ahmad ";
-    console.log(str.slice(0,13))
 }
