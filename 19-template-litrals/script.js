a98f278fbd204b91a36aee3d263108b7126fc745
rehan <rehanahmad3775@gmail.com>
2025-12-28 10:17:08 +0500

    Add examples of template literals and string interpolation in JavaScript

---
+++ 19-template-litrals/script.js
@@ -0,0 +1,16 @@
+{
+    // template litrals 
+    let msg = `Hy there im Rehan Ahmad I'm learning JavaScript `; 
+
+    console.log(msg) //Template literals are a way to create strings in JavaScript using backticks ( ) instead of quotes
+
+}
+
+
+{
+    // string interpolation
+    let user = "Rehan Ahamd";
+    let Course = "JavaScript";
+
+    console.log(`Hello ${user}, you are learning ${Course}`) //String interpolation allows you to insert variables or expressions inside a string using
+}
