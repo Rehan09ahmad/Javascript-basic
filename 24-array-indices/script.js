0fcecb200c620f719e465236b2068c4391e83a41
rehan <rehanahmad3775@gmail.com>
2025-12-29 21:20:26 +0500

    Update array element using index

---
+++ 24-array-indices/script.js
@@ -0,0 +1,7 @@
+// Array indices
+
+let marks = [77, 88, 99, 83, 91, 84, 80];
+console.log(marks[0] = 100)  //Assigning a value to an array index replaces the existing element, and the assignment expression returns the assigned value.
+
+console.log(marks[3])
+
