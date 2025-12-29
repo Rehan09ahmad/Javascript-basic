20e1c6b8ff8cf2e3409e629b8c4e0b7de52852e8
rehan <rehanahmad3775@gmail.com>
2025-12-29 22:44:19 +0500

    Implement array looping using different JavaScript loops

---
+++ 25-looping-of-an-array/script.js
@@ -0,0 +1,32 @@
+// looping of an array
+{
+    // for loop 
+let numbers = [10, 20, 30, 40, 50, 60]
+
+for(let num = 0; num < numbers.length; num++){
+    console.log("index:",numbers[num])   // Index ya arr[i] console.log karte hain.
+}
+
+}
+
+{
+    // for of loop 
+    let hero = ["rehan", "ali", "wijdan", "saad", "sami", "maaz"];
+
+    for(let name of hero ){
+        console.log("Name :", name); // Direct value console.log karte hain.
+        
+    }
+}
+
+{
+    //  do while loop
+    let city = ["Lahore", "Faisalabad", "Rawalpindi", "Multan", "Gujranwala", "Sialkot", "Bahawalpur"];
+     let i = 0;
+
+do {
+  console.log("City Name :",city[i]);
+  i++;
+} while (i < city.length); // for loop and do…while can produce the same output, but do…while executes at least once
+
+}
