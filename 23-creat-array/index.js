0b58520eff95f3bedceb1ab9b1eaabdb54e972a4
rehan <rehanahmad3775@gmail.com>
2025-12-29 13:09:53 +0500

    basic array

---
+++ 23-creat-array/index.js
@@ -0,0 +1,7 @@
+// basic Array  
+ 
+let marks = [97, 91, 88, 80, 75, 70] // Number 
+console.log(marks)
+
+let names = ["Rehan Ahamd", "Waqas Ahmad", "Hmaza Afridi", "Ali Khan"] // string 
+console.log(names)
