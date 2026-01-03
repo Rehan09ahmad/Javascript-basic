ba123831145832d61dbef93c3088359f34b346cd
rehan <rehanahmad3775@gmail.com>
2026-01-03 11:05:39 +0500

    Practice calculating class average using array and loop

--- 26-practice-Question/script.js
+++ 26-practice-Question/script.js
@@ -8,4 +8,19 @@ for(let val of marks ){
 }
 
 let avg = sum / marks.length;
-console.log(` avg marks of the class ${avg}`)  
+console.log(` avg marks of the class ${avg}`);
+
+
+
+
+
+{
+    let marks = [78, 89, 76, 99, 90, 81, 70, 60]
+
+    let sum = 0;
+    for(let val of marks){
+        sum += val;
+    }
+    let avg = sum / marks.length;
+    console.log(`avereg marks of the class ${avg}`);
+}
