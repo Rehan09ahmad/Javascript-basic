483aeea0b44a00f5114f766a9d5c4f9176461c42
rehan <rehanahmad3775@gmail.com>
2026-01-05 22:57:56 +0500

    added arrow function examples with return and console output

---
+++ 31-Arrow-Function/script.js
@@ -0,0 +1,26 @@
+// Arrow Function 
+
+const sum = (a, b) =>{
+    return a + b;
+};
+console.log(sum(40, 88));
+// =============================================================
+
+const multiplication = (a, b) =>{
+    console.log(a * b);
+};
+
+multiplication(10, 9);
+// =============================================================
+
+const devesion = (a, b) =>{
+    return a / b;
+};
+
+console.log(devesion(294, 10))
+
+// =============================
+const greet = (name) => {
+    console.log("Hy there im " + name);
+};
+greet("Rehan Ahmad")
