4dceba98e997282007fa33f1075d6ac5a082bbf7
rehan <rehanahmad3775@gmail.com>
2026-01-08 21:39:06 +0500

    r

---
+++ 33-forEach-Loop/script.js
@@ -0,0 +1,18 @@
+// forEach loop
+
+let arr = [1, 2, 3, 5, 6];
+
+arr.forEach(function value(val){
+        console.log(val)
+
+});
+
+{
+        // callback function
+
+        let arr = [1, 2, 3, 4, 5,];
+        arr.forEach((value,)=>{
+                console.log(value,"callback function")
+        });
+
+}
