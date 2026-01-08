bb224dcc6b8d096afd8f552203bb87db8b2472a2
rehan <rehanahmad3775@gmail.com>
2026-01-08 21:46:45 +0500

    rehan

--- 33-forEach-Loop/script.js
+++ 33-forEach-Loop/script.js
@@ -6,6 +6,12 @@ arr.forEach(function value(val){
         console.log(val)
 
 });
+{
+        let friends = ["Rehan", " Shayan", "Ali", "Rameez"];
+        friends.forEach(function (name){
+                console.log(" Asslam-0-alikom "+ name +" bhai")
+        });
+}
 
 {
         // callback function
