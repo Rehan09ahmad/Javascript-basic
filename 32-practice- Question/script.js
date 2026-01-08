af8a3315f603f80ad70342dfeca7c06661fd3947
rehan <rehanahmad3775@gmail.com>
2026-01-08 17:08:04 +0500

    implemented vowel counter using loop and condition

--- 32-practice- Question/script.js
+++ 32-practice- Question/script.js
@@ -1,10 +1,20 @@
 function countVowel(str) {
     let count = 0;
+    let vowel = " ";
+
     for(const char of str){
-        if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){
-            count++ 
+        if(
+        char === "a" ||
+        char === "e" ||
+        char === "i" ||
+        char === "o" ||
+        char === "u"){
+            count++;
+            vowel += char +" ";
         }
     }
-    return count
+    console.log("vowel", vowel)
+    return count;
 };
 
+console.log("total", countVowel("rehan ahmad"))
