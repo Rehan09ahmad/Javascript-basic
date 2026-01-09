ea85ddfbe32627f461c577f6af2e7b0686f63098
rehan <rehanahmad3775@gmail.com>
2026-01-09 21:44:23 +0500

    r

--- 35-map-methos/scripnt.js
+++ 35-map-methos/scripnt.js
@@ -1,6 +1,6 @@
 // simpal methos map
 {
-    let nums = [1, 2, 3, 4, 5];
+    let nums = [10, 20, 30, 40, 50];
 
 nums.map((val)=>{
     console.log(val + 5) // add 5 with every index 
@@ -10,10 +10,31 @@ nums.map((val)=>{
 // add newarray in map 
 
 {
-    let nums = [1, 2, 3, 4, 5];
+    let nums = [5, 10, 15, 20, 25];
 
     let NewArr = nums.map((val)=>{
         return val * 2; //  multiplay 2 with every index  
     })
     console.log(NewArr)
 }
+
+
+// filter array 
+{
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    let EvenNumber = numbers.filter((val) =>{
+        return val % 2 === 0;
+    });
+    console.log(EvenNumber)
+}
+
+// filter array Students Marks 
+
+let marks = [44, 59 , 40, 70, 90, 60];
+
+let passMarks = marks.filter((val)=>{
+    return val > 44;
+});
+
+console.log(passMarks)
