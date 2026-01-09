67eb2e730195e621eb9fa8c3abeab89ba709c786
rehan <rehanahmad3775@gmail.com>
2026-01-09 13:59:26 +0500

    rehan

---
+++ 35-map-methos/scripnt.js
@@ -0,0 +1,19 @@
+// simpal methos map
+{
+    let nums = [1, 2, 3, 4, 5];
+
+nums.map((val)=>{
+    console.log(val + 5) // add 5 with every index 
+})
+}
+
+// add newarr in map 
+
+{
+    let nums = [1, 2, 3, 4, 5];
+
+    let NewArr = nums.map((val)=>{
+        return val * 2; //  multiplay 2 with every index  
+    })
+    console.log(NewArr)
+}
