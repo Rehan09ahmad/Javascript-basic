9140dd0fdb0f5ca716d8578befe45580fc1bcdf9
rehan <rehanahmad3775@gmail.com>
2026-01-23 21:55:05 +0500

    rehan

---
+++ 39-DOM-Practice-2/script.js
@@ -0,0 +1,7 @@
+let newBtn = document.createElement("button");
+newBtn.innerText = "Click me!";
+
+newBtn.style.color = "White";
+newBtn.style.backgroundColor = "red";
+
+document.querySelector("body").prepend(newBtn)
