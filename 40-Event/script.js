c0d785e68a7ce54ab086c0253a1747fc91c1cf6d
rehan <rehanahmad3775@gmail.com>
2026-01-28 22:47:25 +0500

    event handler

--- 40-Event/script.js
+++ 40-Event/script.js
@@ -3,3 +3,9 @@ let button = document.getElementById("button");
 button.style.color = "white";
 button.style.background = "black";
 
+let Btn = document.querySelector("#Btn");
+
+Btn.onclick = () => {
+    console.log("Btn was click ");
+};
+
