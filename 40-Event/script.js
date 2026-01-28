7069901aa53f2ca8e0ba406fd24a4bbc19e5faa6
rehan <rehanahmad3775@gmail.com>
2026-01-29 00:45:52 +0500

    event handling

--- 40-Event/script.js
+++ 40-Event/script.js
@@ -1,11 +1,19 @@
-let button = document.getElementById("button");
+// let button = document.getElementById("button");
 
-button.style.color = "white";
-button.style.background = "black";
+// button.style.color = "white";
+// button.style.background = "black";
 
-let Btn = document.querySelector("#Btn");
+// let Btn = document.querySelector("#Btn");
 
 Btn.onclick = () => {
     console.log("Btn was click ");
 };
 
+let button = document.querySelector("button");
+
+button.onclick = (evt) =>{
+    console.log(evt);
+    console.log(evt.type);
+    console.log(evt.target);
+
+}
